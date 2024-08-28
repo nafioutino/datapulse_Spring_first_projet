d9e278f5fe8f9a0e17f618311bbb96175e3afd18
Abdoul-Nafiou <nafiou531@gmail.com>
2024-08-28 18:36:09 +0100

    bdd connexion

--- cours.md
+++ cours.md
@@ -8,3 +8,13 @@ Elle sert à marquer une classe comme étant un repository, c'est-à-dire un end
 ## Est-ce obligatoire ?
 
 Non ! Quand on implémente déjà une interface comme `JpaRepository` , Spring détecte automatiquement notre repository.
+
+
+## @PathVariable vs @RequestBody vs @Requestparam
+
+### @PathVariable
+On l'utilise pour extraire une partie variable dans notre chemin d'URL.
+### @RequestBody
+On l'utilise quand on reçoit des donées via post.
+###  @Requestparam
+On l'utilise quand on a des paramètres dans l'URL  sous la forme `?param=valeur`.
--- src/main/java/bj/higfiveuniversity/datapulse/controller/ProductController.java
+++ src/main/java/bj/higfiveuniversity/datapulse/controller/ProductController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,12 +31,12 @@ public class ProductController {
 
 
     @PostMapping
-    public Product createProduct(Product product) {
+    public Product createProduct( @RequestBody Product product) {
         return productRepository.save(product);   
     }
 
     @PutMapping("/{id}")
-    public Product updateProduct(Long id, Product productDetail) {
+    public Product updateProduct( @PathVariable Long id, @RequestBody Product productDetail) {
         Product product = productRepository.findById(id).orElseThrow();
         product.setName(productDetail.getName());
         product.setPrice(product.getPrice());
@@ -42,7 +44,7 @@ public class ProductController {
     }
     
     @DeleteMapping("/{id}")
-    public void deleteProduct(Long id) {
+    public void deleteProduct(@PathVariable Long id) {
         Product product = productRepository.findById(id).orElseThrow();
         productRepository.delete(product);   
     }
